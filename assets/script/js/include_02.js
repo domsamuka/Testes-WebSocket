94a9f4b191509f8da85245ab9d671616ea88f24e
DomSamuka <DomSamuka@hotmail.com>
2019-12-09 14:07:10 -0300

    all

---
+++ assets/script/js/include_02.js
@@ -0,0 +1,15 @@
+// "use strict";
+
+// Object.defineProperty( exports , "MyExports", { value: true } );
+
+class MeusTestes
+{
+    constructor()
+    {
+        console.log( ' # MeusTestes.constructor = OK ... ' );
+    }
+}
+
+// exports.default = MeusTestes ;
+
+new MeusTestes() ;
---
+++ index.js
@@ -0,0 +1,7 @@
+
+	const vPORT = process.env.PORT || 3333 ;
+
+	const express = require( 'express' )() ;
+
+	const vInclude = require( "./include.js" ) ;
+
